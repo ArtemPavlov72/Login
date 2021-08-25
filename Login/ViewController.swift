60b14d4d3baae804a6d07cb1beaf8be7d8e4ff61
Artem Pavlov <pavlov.tema@gmail.com>
2021-08-25 19:48:11 +0500

    alarm actions are work

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var userName: UITextField!
+    @IBOutlet var password: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func logInPressed() {
+        guard userName.text == "Artem", password.text == "0000" else {
+            showAlert(
+                with: "Invalid login or password",
+                and: "Please, enter correct login and password"
+            )
+            return
+        }
+        
+    }
+    
+    @IBAction func forgotUserPressed() {
+        showAlert(with: "Oops!", and: "Your name is Artem 😉")
+    }
+    
+    @IBAction func forgotPasswordPressed() {
+        showAlert(with: "Oops!", and: "Your password is 0000 😃")
+    }
+    
+    
 }
 
+// MARK: - Alarm Methods
+extension ViewController {
+    private func showAlert(with title: String, and massage:String) {
+        let alert = UIAlertController(
+            title: title,
+            message: massage,
+            preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+    }
+}
