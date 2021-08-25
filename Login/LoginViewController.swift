c5a43a039dea4e62d45264aafb76963d4fcc45d1
Artem Pavlov <pavlov.tema@gmail.com>
2021-08-26 01:23:54 +0500

    Gradient add for WelcomeViewController

--- Login/LoginViewController.swift
+++ Login/LoginViewController.swift
@@ -65,3 +65,5 @@ extension LoginViewController {
         present(alert, animated: true)
     }
 }
+
+
--- Login/WelcomeViewController.swift
+++ Login/WelcomeViewController.swift
@@ -15,6 +15,15 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        welcomeUserName.text = "Welcome \(nameForWelcome!)!"
+        
+        welcomeUserName.text = "Welcome \(nameForWelcome ?? "Wrong name")!"
+        
+        let gradient = CAGradientLayer()
+        gradient.frame = view.bounds
+        let colotTop = UIColor.init(red: 190.0/255.0, green: 127.0/255.0, blue: 140.0/255.0, alpha: 1)
+        let colorBottom = UIColor.init(red: 105.0/255.0, green: 125.0/255.0, blue: 173.0/255.0, alpha: 1)
+        gradient.colors = [colotTop.cgColor, colorBottom.cgColor]
+        
+        view.layer.insertSublayer(gradient, at: 0)
     }
 }
